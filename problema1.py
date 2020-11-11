203b713809bea07ded05a21a470d471a65534ad0
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-11-11 12:33:11 +0300

    Add files via upload

---
+++ problema1.py
@@ -0,0 +1,9 @@
+n=eval(input("Introduceti numarul de zile n= "))
+if n==28:
+    print("Februarie")
+if n==29:
+    print("Februarie an bisect")
+if n==30:
+    print("Septembrie,Noiembrie,Aprilie,Iunie")
+if n==31:
+    print("Octombrie, Ianuarie,Decembrie, Martie, Mai, Iulie, August")
