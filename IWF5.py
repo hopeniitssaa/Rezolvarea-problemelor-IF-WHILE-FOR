c915d16c7909dcbaa6c825215037677d4b475c0f
Băbuță Luminița <70962140+hopeniitssaa@users.noreply.github.com>
2020-11-19 21:46:55 +0300

    Add files via upload

---
+++ IWF5.py
@@ -0,0 +1,25 @@
+an = int(input("Dati un an: "))
+if (an % 12 == 0):
+    print("Maimuta")
+if (an % 12 == 1):
+    print("Cocos")
+if (an % 12 == 2):
+    print("Caine")
+if (an % 12 == 3):
+    print("Porc")
+if (an % 12 == 4):
+    print("Sobolan")
+if (an % 12 == 5):
+    print("Bou")
+if (an % 12 == 6):
+    print("Tigru")
+if (an % 12 == 7):
+    print("Iepure")
+if (an % 12 == 8):
+    print("Dragon")
+if (an % 12 == 9):
+    print("Sarpe")
+if (an % 12 == 10):
+    print("Cal")
+if (an % 12 == 11):
+    print("Oaie") 
